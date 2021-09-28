bc15c7de42b6ed1b5d27638c1a89a067a1d59b4d
Omico <omico@qq.com>
2021-09-27 19:28:25 -0700

    [API] Fix Deserialization issue

--- api/src/main/java/co/railgun/api/bgmrip/model/Response.kt
+++ api/src/main/java/co/railgun/api/bgmrip/model/Response.kt
@@ -6,15 +6,13 @@ import kotlinx.serialization.Serializable
 internal interface Response
 
 @Serializable
-abstract class DataResponse<T> : Response {
-
-    @SerialName("data")
-    abstract val data: T
-}
+data class DataResponse<T>(
+    @SerialName("data") val data: T,
+) : Response
 
 @Serializable
 data class ListDataResponse<T>(
-    @SerialName("data") override val data: List<T>,
+    @SerialName("data") val data: List<T>,
     @SerialName("count") val count: Int = 0,
     @SerialName("status") val status: Int = 0,
-) : DataResponse<List<T>>()
+) : Response
