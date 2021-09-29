ce3a0e372940cf10244f3a57a82690bad22669ec
Omico <omico@qq.com>
2021-09-29 03:51:03 -0700

    [API] Cleanup build.gradle.kts

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -13,18 +13,15 @@ android {
 }
 
 dependencies {
-    compileOnly("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
-    compileOnly("com.squareup.retrofit2:retrofit:2.9.0")
-    compileOnly("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.5.2")
-    compileOnly("org.jetbrains.kotlinx:kotlinx-serialization-json:1.2.2")
-    compileOnly("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:0.8.0")
-
-    compileOnly("me.omico.cloudflare.api:cloudflare-dns:1.0.0-SNAPSHOT")
-
-    compileOnly("com.squareup.retrofit2:converter-scalars:2.9.0")
-    debugImplementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
-
-    compileOnly("androidx.datastore:datastore:1.0.0")
-    compileOnly("com.google.protobuf:protobuf-kotlin:3.18.0")
-    compileOnly("me.omico.cryonics:cryonics:1.0.0-SNAPSHOT")
+    api(androidx.datastore.datastore)
+    api(kotlinx.coroutines.core)
+    api(kotlinx.serialization.json)
+    api(okhttp4.interceptor.logging)
+    api(okhttp4.okhttp)
+    api(omico.cloudflareApi.dns)
+    api(omico.cryonics.cryonics)
+    api(protobuf3.protobufKotlin)
+    api(retrofit2.converter.kotlinxSerialization)
+    api(retrofit2.converter.scalars)
+    api(retrofit2.retrofit)
 }
