a12aadff87e0bd0cbaef9ae43290bc976e2f1563
Omico <omico@qq.com>
2021-09-29 04:41:09 -0700

    [UI] main: Disable window decor fitting

--- ui/main/src/main/java/co/railgun/spica/ui/main/MainActivity.kt
+++ ui/main/src/main/java/co/railgun/spica/ui/main/MainActivity.kt
@@ -3,11 +3,13 @@ package co.railgun.spica.ui.main
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.WindowCompat
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        WindowCompat.setDecorFitsSystemWindows(window, false)
         setContent { MainUI() }
     }
 }
