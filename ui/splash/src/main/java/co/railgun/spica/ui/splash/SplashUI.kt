8512465609e72ede9d3a5b77d85436a7885c1250
Omico <omico@qq.com>
2021-10-01 07:20:11 -0700

    [UI] splash: Show app icon

--- ui/splash/src/main/java/co/railgun/spica/ui/splash/SplashUI.kt
+++ ui/splash/src/main/java/co/railgun/spica/ui/splash/SplashUI.kt
@@ -1,9 +1,15 @@
 package co.railgun.spica.ui.splash
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.lifecycle.viewmodel.compose.viewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
@@ -18,5 +24,14 @@ fun SplashUI(
         if (route == SplashScreen.route) return@LaunchedEffect
         navController.popBackStack()
         navController.navigate(route)
+    }
+    Box(
+        modifier = Modifier.fillMaxSize(),
+        contentAlignment = Alignment.Center
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.ic_launcher_foreground),
+            contentDescription = null
+        )
     }
 }
