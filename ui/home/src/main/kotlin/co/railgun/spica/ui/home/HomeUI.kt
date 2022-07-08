228ed25067f7ccacc9392385260dad5db341a8a6
Omico <me@omico.icu>
2022-07-08 05:53:38 -0700

    [UI] home: Show text while no data

--- ui/home/src/main/kotlin/co/railgun/spica/ui/home/HomeUI.kt
+++ ui/home/src/main/kotlin/co/railgun/spica/ui/home/HomeUI.kt
@@ -1,15 +1,18 @@
 package co.railgun.spica.ui.home
 
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.LazyListState
 import androidx.compose.foundation.lazy.rememberLazyListState
+import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.lifecycle.viewmodel.compose.viewModel
@@ -97,6 +100,15 @@ private fun HomeContent(
     val myBangumi by rememberUpdatedState(newValue = uiState.myBangumi)
     val onAir by rememberUpdatedState(newValue = uiState.onAir)
     val onItemClick: Bangumi.() -> Unit = { onSubmitAction(HomeAction.NavigateToBangumiDetail(id)) }
+    if (announcedBangumi.isEmpty() && myBangumi.isEmpty() && onAir.isEmpty()) {
+        Box(
+            modifier = modifier,
+            contentAlignment = Alignment.Center,
+        ) {
+            Text("No data")
+        }
+        return
+    }
     LazyColumn(
         modifier = modifier,
         state = lazyListState,
